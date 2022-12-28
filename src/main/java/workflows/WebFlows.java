e7e4be1c94cd1cad8eb052d83705516c291012b9
sbenhamou <107076616+sbenhamou@users.noreply.github.com>
2022-12-28 21:23:14 +0200

    Update WebFlows.java

--- src/main/java/workflows/WebFlows.java
+++ src/main/java/workflows/WebFlows.java
@@ -17,7 +17,6 @@ public class WebFlows extends CommonOps {
         UIActions.updateText(contactUsPage.txt_email, email);
         UIActions.updateText(contactUsPage.txt_mess, message);
         UIActions.click(contactUsPage.but_submit);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
 
     @Step("Business Flow: Submit message on Contact us with DB data")
@@ -28,17 +27,14 @@ public class WebFlows extends CommonOps {
         UIActions.updateText(contactUsPage.txt_email, cred.get(1));
         UIActions.updateText(contactUsPage.txt_mess, cred.get(2));
         UIActions.click(contactUsPage.but_submit);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
 
     @Step("Business Flow: Search products in the store")
     public static void getProductList(String product)
     {
         UIActions.click(fixMenu.menu_store);
-//        UIActions.updateTextHuman(storePage.txt_search,product);
         UIActions.updateText(storePage.txt_search,product);
         UIActions.click(storePage.btn_search);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
 
     @Step("Business Flow: Choose item in the store")
